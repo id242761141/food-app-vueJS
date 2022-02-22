a286ff35c400f5c8ecf0d9675a7590e8bb563886
Assylbek <id242761141@gmail.com>
2022-02-22 13:56:26 +0500

    test timur

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/basket',
+    name: 'basket',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "basket" */ '../views/BasketView.vue')
   }
 ]
 
---
+++ src/views/BasketView.vue
@@ -0,0 +1,24 @@
+<template>
+
+<div class="basket">
+    <img src="https://www.pngitem.com/pimgs/m/536-5362520_stiker-fuck-you-hd-png-download.png" alt="">
+    <div class="text">
+        TimurZhan
+    </div>
+</div>
+
+</template>
+
+<style lang="less">
+    .basket{
+        margin: 20px;
+        .text{
+            text-align: center;
+            font-size: 30px;
+            margin-top: 30px;
+        }
+        img{
+            width: 100%;
+        }
+    }
+</style>
