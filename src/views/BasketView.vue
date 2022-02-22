0ad606e46de15189d638c68abcc7c193b170bdb4
Assylbek <id242761141@gmail.com>
2022-02-22 17:19:23 +0500

    edit error Basket pages

--- src/views/BasketView.vue
+++ src/views/BasketView.vue
@@ -7,15 +7,5 @@
 </template>
 
 <style lang="less">
-    .basket{
-        margin: 20px;
-        .text{
-            text-align: center;
-            font-size: 30px;
-            margin-top: 30px;
-        }
-        img{
-            width: 100%;
-        }
-    }
+    
 </style>
